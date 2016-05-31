93e471c507d45355dae6e103417be3d49ccf23d4
Leandro <contato@leandrocazarini.com>
2016-05-31 05:25:18 +0000

    altercao no botao de login da view welcome

--- AcademiaWeb/resources/views/welcome.blade.php
+++ AcademiaWeb/resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
                     @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Entrar</a></li>
+                        <li><a href="{{ url('/home') }}">Entrar</a></li>
                         <li><a href="{{ url('/register') }}">Cadastrar</a></li>
                         
                         
